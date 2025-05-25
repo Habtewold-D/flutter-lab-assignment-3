bd522cc2bb9333f8c16e30c074e37f8705349b18
Habtewold-D <habtewold21degfie.com>
2025-05-24 15:55:34 -1100

    album and photo models created

---
+++ lib/data/models/album.dart
@@ -0,0 +1,23 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:equatable/equatable.dart';
+
+part 'album.g.dart';
+
+@JsonSerializable()
+class Album extends Equatable {
+  final int id;
+  final int userId;
+  final String title;
+
+  const Album({
+    required this.id,
+    required this.userId,
+    required this.title,
+  });
+
+  factory Album.fromJson(Map<String, dynamic> json) => _$AlbumFromJson(json);
+  Map<String, dynamic> toJson() => _$AlbumToJson(this);
+
+  @override
+  List<Object?> get props => [id, userId, title];
+} 
